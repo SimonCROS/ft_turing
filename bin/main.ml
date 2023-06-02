917572b083622b08e62c0bbbe7263e057fbe3c09
SimonCROS <scros@student.42lyon.fr>
2023-06-02 16:00:00 +0200

    get name in json

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,18 @@
-open Core;;
-open Yojson;;
+open Yojson.Basic.Util
 
-let () = print_endline "Hello, World!"
+let get_name_value json =
+  let name = member "name" json |> to_string in
+  name
+
+let () =
+  let filename = "machines/unary_sub.subject.json" in
+  try
+    let ic = open_in filename in
+    let json = Yojson.Basic.from_channel ic in
+    close_in ic;
+
+    Format.printf "Found value: %s\n" (get_name_value json);
+
+    flush stdout;
+  with e ->
+    raise e;
