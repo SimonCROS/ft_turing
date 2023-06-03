1de13505a03379e5f6dc84543564ed67fe5e80be
Simon CROS <simon.dc.cros@gmail.com>
2023-06-04 00:13:12 +0200

    Start parsing

--- bin/main.ml
+++ bin/main.ml
@@ -1,16 +1,40 @@
-let get_name_value json =
-  let name = Yojson.Basic.Util.member "name" json |> Yojson.Basic.Util.to_string in
-  name
+type transition =
+{read : char;
+ to_state : string;
+ write : char;
+ action : string} [@@deriving of_yojson]
+
+type machine =
+{name : string;
+ alphabet : char list;
+ blank : char;
+ states : string list;
+ initial : string;
+ finals : string list;
+ transitions : Yojson.Safe.t} [@@deriving of_yojson]
 
 let () =
-  (* TODO Check if param exists *)
+  if Array.length Sys.argv != 2 then begin
+    failwith "Wrong amount of arguments. Required <filename>.";
+  end;
+
   let filename = Sys.argv.(1) in
   try
     let ic = open_in filename in
-    let json = Yojson.Basic.from_channel ic in
+    let json = Yojson.Safe.from_channel ic in
     close_in ic;
 
-    Format.printf "\nFound value: %s\n" (get_name_value json);
+    let machine = match machine_of_yojson json with
+    | Ok data -> data;
+    | Error err -> failwith ("Invalid machine : parsing failed for " ^ err);
+    in
+
+    Format.printf "Machine name : %s\n" machine.name;
+    Format.printf "Machine alphabet :";
+    List.iter (Format.printf " `%c`") machine.alphabet;
+    Format.printf "\n";
+
+    Format.printf "Machine transitions : %a" Yojson.Safe.pp machine.transitions;
 
     flush stdout;
   with e ->
