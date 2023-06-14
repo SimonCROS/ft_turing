02f51474aa7f7c4c3e94221897d6a37534fcb1cd
Karim Zennoune <zennoune.karim@outlook.fr>
2023-06-14 15:40:12 +0200

    save

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
 pub mod machine_parser;
+use std::fs;
 
 fn main() {
 
-    let result = machine_parser::typed_example();
+    // let result = machine_parser::typed_example();
+
+
+    let file_path = "machines/is_0n1n.json".to_owned();
+    let contents = fs::read_to_string(file_path).expect("Couldn't find or load that file.");
+
+    // parser::untyped_example(&contents);
+    let result = machine_parser::typed_example(&contents);
 
     match result {
         Ok(_) => (),
         Err(error) => print!("{}", error),
     }
 }
+
+// println!("Hello, world!");
+
+//     // Grab JSON file
+//     let file_path = "data/test.json".to_owned();
+//     let contents = fs::read_to_string(file_path).expect("Couldn't find or load that file.");
+
+//     parser::untyped_example(&contents);
