e29e901d00496945097cf97e1df8991808dec6ee
SimonCROS <scros@student.42lyon.fr>
2023-06-16 11:58:43 +0200

    use format to pass string

--- src/machine_parser.rs
+++ src/machine_parser.rs
@@ -1,6 +1,5 @@
 use serde::{Deserialize, Serialize};
-use serde_json::Result;
-use std::collections::HashMap;
+use std::{collections::HashMap};
 
 #[derive(Serialize, Deserialize)]
 pub struct Transition {
@@ -19,11 +18,6 @@ pub struct Machine {
     initial: String,
     finals: Vec<String>,
     transitions: HashMap<String, Vec<Transition>>,
-}
-
-pub fn machine_parser(data: &str) -> Result<Machine> {
-    let m: Machine = serde_json::from_str(data)?;
-    return Ok(m);
 }
 
 pub fn machine_printer(m: &Machine) -> () {
@@ -72,7 +66,7 @@ pub fn machine_printer(m: &Machine) -> () {
     }
 }
 
-pub fn machine_checker(m: &Machine) -> Option<&'static str> {
+pub fn machine_checker(m: &Machine) -> Option<String> {
     // -Your program must detect and reject ill formated or invalid machine descriptions
     // and inputs, with a relevant error message. This means that your program must
     // never crash for any reason.
@@ -81,24 +75,24 @@ pub fn machine_checker(m: &Machine) -> Option<&'static str> {
     for i in 0..(m.alphabet.len() - 1) {
         for j in (i + 1)..(m.alphabet.len()) {
             if m.alphabet[i] == m.alphabet[j] {
-                println!("duplicate alphabet symbol [{}]", m.alphabet[i]);
-                return Some("Machine logic check error");
+                return Some(format!("duplicate alphabet symbol [{}]", m.alphabet[i]));
             }
         }
     }
 
     // -blank: The blank character, must be part of the alphabet
     if m.alphabet.iter().position(|x| x == &m.blank) == None {
-        println!("blank symbol [{}] is not part of alphabet", m.blank);
-        return Some("Machine logic check error");
+        return Some(format!(
+            "blank symbol [{}] is not part of alphabet",
+            m.blank
+        ));
     }
 
     //states cannot be duplicate
     for i in 0..(m.states.len() - 1) {
         for j in (i + 1)..(m.states.len()) {
             if m.states[i] == m.states[j] {
-                println!("duplicate state [{}]", m.states[i]);
-                return Some("Machine logic check error");
+                return Some(format!("duplicate state [{}]", m.states[i]));
             }
         }
     }
@@ -108,8 +102,10 @@ pub fn machine_checker(m: &Machine) -> Option<&'static str> {
 
     //initial
     if m.states.iter().position(|x| x == &m.initial) == None {
-        println!("initial state [{}] is not part of states", m.initial);
-        return Some("Machine logic check error");
+        return Some(format!(
+            "initial state [{}] is not part of states",
+            m.initial
+        ));
     }
 
     //finals
@@ -117,8 +113,7 @@ pub fn machine_checker(m: &Machine) -> Option<&'static str> {
         match item {
             _ => {
                 if m.states.iter().position(|x| x == item) == None {
-                    println!("final state [{}] is not part of states", item);
-                    return Some("Machine logic check error");
+                    return Some(format!("final state [{}] is not part of states", item));
                 }
             }
         }
@@ -132,8 +127,7 @@ pub fn machine_checker(m: &Machine) -> Option<&'static str> {
                 if m.finals.iter().position(|x| x == item) == None {
                     //state is not final, check if it is in transition
                     if !m.transitions.contains_key(item) {
-                        println!("state [{}] has no transitions.", item);
-                        return Some("Machine logic check error");
+                        return Some(format!("state [{}] has no transitions.", item));
                     }
                 }
             }
@@ -143,8 +137,7 @@ pub fn machine_checker(m: &Machine) -> Option<&'static str> {
     // -make sure every transition block is for a state that exists
     for (key, _value) in &m.transitions {
         if m.states.iter().position(|x| x == key) == None {
-            println!("transition [{}] is not part of states", key);
-            return Some("Machine logic check error");
+            return Some(format!("transition [{}] is not part of states", key));
         }
     }
 
@@ -162,11 +155,10 @@ pub fn machine_checker(m: &Machine) -> Option<&'static str> {
             //check double read on same symbol
             for j in (i + 1)..(value.len()) {
                 if value[i].read == value[j].read {
-                    println!(
+                    return Some(format!(
                         "in transition [{}] there is multiple statement for read [{}]",
                         key, value[i].read
-                    );
-                    return Some("Machine logic check error");
+                    ));
                 }
             }
 
@@ -177,46 +169,42 @@ pub fn machine_checker(m: &Machine) -> Option<&'static str> {
 
             // -action can only be RIGHT or LEFT
             if !(value[i].action == "LEFT" || &value[i].action == "RIGHT") {
-                // println!("Wrong action in [{}] read [{}] value [{}] should be [RIGHT] or [LEFT] only", key, value[i].read, value[i].action);
-                println!(
+                return Some(format!(
                     "statement action [{}] from [{}] read [{}] should be [RIGHT] or [LEFT] only",
                     value[i].action, key, value[i].read
-                );
-                return Some("Machine logic check error");
+                ));
             }
 
             // -make sure "to_state" statements use only stuff from "states"
             if m.states.iter().position(|x| x == &value[i].to_state) == None {
-                println!(
+                return Some(format!(
                     "statement to_state [{}] from [{}] read [{}] is not part of states",
                     value[i].to_state, key, value[i].read
-                );
-                return Some("Machine logic check error");
+                ));
             }
 
             // -make sure read/write statements use only stuff from "alphabet"
             //read
             if m.alphabet.iter().position(|x| x == &value[i].read) == None {
-                println!(
+                return Some(format!(
                     "statement read [{}] from [{}] read [{}] is not part of alphabet",
                     value[i].read, key, value[i].read
-                );
-                return Some("Machine logic check error");
+                ));
             }
 
             //write
             // if m.alphabet.iter().position(|x| x == &value[i].write) == None
             // {
-            //     println!("statement wirte [{}] from [{}] read [{}] is not part of alphabet", value[i].write, key, value[i].read);
-            //     return Some("Machine logic check error");
+            //     return Some(format!("statement wirte [{}] from [{}] read [{}] is not part of alphabet", value[i].write, key, value[i].read));
             // }
         } //end for item in value.iter()
     }
 
     // -at least one HALT statement must be present in a to_state statement
     if !found_final {
-        println!("at least one final state must be present in the to_state statements");
-        return Some("Machine logic check error");
+        return Some(format!(
+            "at least one final state must be present in the to_state statements"
+        ));
     }
 
     return None;
--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,21 @@ fn main() {
     let file_path = "machines/unary_sub.subject.json".to_owned();
     let contents = fs::read_to_string(file_path).expect("Couldn't find or load that file.");
 
-    let machine = machine_parser::machine_parser(&contents);
+    let machine = serde_json::from_str(&contents);
 
     match machine {
-        Err(error) => print!("{}", error),
         Ok(m) => {
             println!("json syntax OK");
             machine_parser::machine_printer(&m);
             let checker = machine_parser::machine_checker(&m);
             match checker {
-                Some(error) =>{ println!("{}", error); return ();},
+                Some(error) => { println!("{}", error); return ();},
                 None => {
                     println!("logic check OK");
                 },
             }
         },
+        Err(error) => print!("{}", error),
     }
 
 
