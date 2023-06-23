8992ea2af980cfa3cc76a4f5f5c18fa93f7dc33c
SimonCROS <scros@student.42lyon.fr>
2023-06-23 09:14:59 +0200

    Change to a more functionnal way for printing

--- src/print.rs
+++ src/print.rs
@@ -1,4 +1,4 @@
-use crate::machine_parser::Machine;
+use crate::machine_parser::{Machine, Transition};
 
 pub fn print_help() {
     println!("usage: ft_turing [-h] jsonfile input");
@@ -19,42 +19,20 @@ pub fn print_output_header() {
     println!("{:^15}|{:^23}|{:^3}|{:^23}|{:^3}|{:^8}", "Ribbon", "Current State", "R", "New State", "W", "action");
 }
 
+fn print_transitions(name: &String, transitions: &Vec<Transition>) {
+    println!("{:#^60}", name);
+    transitions.iter().for_each(|item| println!("{: ^12}|{: ^24}|{: ^12}|{: ^12}", item.read, item.to_state, item.write, item.action));
+}
+
 pub fn print_machine(m: &Machine) {
     println!("{:^60}", "machine name:");
     println!("{:^60}", m.name);
-    print!("{:<10}", "alphabet:");
-    for item in m.alphabet.iter() {
-        match item {
-            _ => print!("[{}] ", item),
-        }
-    }
-    println!("");
-    print!("{:<10}", "blank:");
-    println!("[{}]", m.blank);
-    print!("{:<10}", "states: ");
-    for item in m.states.iter() {
-        match item {
-            _ => print!("[{}] ", item),
-        }
-    }
-    println!("");
-    print!("{:<10}", "initial:");
-    println!("[{}]", m.initial);
-    print!("{:<10}", "finals: ");
-    for item in m.finals.iter() {
-        match item {
-            _ => print!("[{}] ", item),
-        }
-    }
-    println!("");
+    println!("{:<10}{}", "alphabet: ", m.alphabet.iter().fold(String::new(), |acc, item| format!("{} [{}]", acc, item)));
+    println!("{:<10} [{}]", "blank:", m.blank);
+    println!("{:<10}{}", "states: ", m.states.iter().fold(String::new(), |acc, item| format!("{} [{}]", acc, item)));
+    println!("{:<10} [{}]", "initial:", m.initial);
+    println!("{:<10}{}", "finals: ", m.finals.iter().fold(String::new(), |acc, item| format!("{} [{}]", acc, item)));
     println!("{:^60}", "transitions:");
     println!("{:^12 }|{: ^24}|{: ^12}|{: ^12}", "read", "to_state", "write", "action");
-    for (key, value) in m.transitions.iter() {
-        println!("{:#^60}", key);
-        for item in value.iter() {
-            match item {
-                _ => println!("{: ^12}|{: ^24}|{: ^12}|{: ^12}", item.read, item.to_state, item.write, item.action),
-            }
-        }
-    }
+    m.transitions.iter().for_each(|(k, v)| print_transitions(k, v));
 }
