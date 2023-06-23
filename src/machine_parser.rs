f3ff554312632bdf18b50a51653fcac3bcc51c10
SimonCROS <scros@student.42lyon.fr>
2023-06-23 14:16:46 +0200

    Finish all parsing tests

--- src/machine_parser.rs
+++ src/machine_parser.rs
@@ -30,6 +30,14 @@ where
     }
 }
 
+fn check_duplicates_on<T, F: Fn(&T, &T) -> bool>(list: &Vec<T>, f: F) -> Option<usize>
+{
+    match (1..list.len()).find(|i| list[*i..].iter().find(|el| f(&list[i - 1], el)).is_some()) {
+        Some(duplicated) => Some(duplicated),
+        None => None,
+    }
+}
+
 fn check_contains_all<T>(needle: &Vec<T>, haystack: &Vec<T>) -> Option<T>
 where
     T: PartialEq + Clone,
@@ -42,34 +50,47 @@ where
 
 pub fn machine_checker(m: &Machine) -> Result<(), String> {
     if m.alphabet.len() < 2 {
-        // Error if the alphabet has less than 2 symbols (blank + 1 other minimum)
-        Err(format!("a minimum of 2 alphabet symbol are required"))
+        Err(format!("a minimum of 2 alphabet symbol are required (blank + 1 other minimum)"))
     } else if m.states.len() < 2 {
-        // Error if there are less than 2 states (initial + 1 final minimum)
-        Err(format!("a minimum of 2 states are required"))
+        Err(format!("a minimum of 2 states are required (initial + 1 final minimum)"))
     } else if m.finals.len() < 1 {
-        // Error if there is no final state
         Err(format!("at least one final state is required"))
     } else if !m.alphabet.contains(&m.blank) {
-        // Error if the blank character is not part of the alphabet
         Err(format!("blank symbol [{}] is not part of alphabet", m.blank))
     } else if !m.states.contains(&m.initial) {
-        // Error if the initial state is not in the states list
         Err(format!("initial state [{}] is not part of states", m.initial))
     } else if m.finals.contains(&m.initial) {
-        // Error if the initial state is a final state
         Err(format!("initial state [{}] is also a final state", m.initial))
     } else if let Some(not_in) = check_contains_all(&m.finals, &m.states) {
-        // Error if a final state is not in the states list
         Err(format!("final state [{}] is not part of states", not_in))
     } else if let Some(duplicated) = check_duplicates(&m.alphabet) {
-        // Error if there is a duplicated alphabet symbol
         Err(format!("duplicate alphabet symbol [{}]", duplicated))
     } else if let Some(duplicated) = check_duplicates(&m.states) {
-        // Error if there is a duplicated state
         Err(format!("duplicate state [{}]", duplicated))
     } else {
         check_transitions(m)
+    }
+}
+
+fn check_transition(m: &Machine, name: &String, transition: &Transition) -> Result<(), String> {
+    if !matches!(transition.action.as_str(), "LEFT" | "RIGHT") {
+        Err(format!("statement action [{}] from [{}] read [{}] should be [RIGHT] or [LEFT] only", transition.action, name, transition.read))
+    } else if !m.states.contains(&transition.to_state) {
+        Err(format!("statement to_state [{}] from [{}] read [{}] is not part of states", transition.to_state, name, transition.read))
+    } else if !m.alphabet.contains(&transition.read) {
+        Err(format!("statement read [{}] from [{}] read [{}] is not part of alphabet", transition.read, name, transition.read))
+    // } else if !m.alphabet.contains(&transition.write) {
+    //     Err(format!("statement write [{}] from [{}] read [{}] is not part of alphabet", transition.write, name, transition.read))
+    } else {
+        Ok(())
+    }
+}
+
+fn check_state_transitions(m: &Machine, name: &String, transitions: &Vec<Transition>) -> Result<(), String> {
+    if let Some(duplicated_index) = check_duplicates_on(transitions, |l, r| l.read == r.read) {
+        Err(format!("multiple statement for read [{}] in transition [{}]", transitions[duplicated_index].read, name))
+    } else {
+        transitions.iter().try_for_each(|item| check_transition(m, name, item))
     }
 }
 
@@ -78,54 +99,16 @@ fn check_transitions(m: &Machine) -> Result<(), String> {
     let not_final_states: Vec<String> = m.states.iter().filter(|el| !m.finals.contains(el)).cloned().collect();
 
     if let Some(duplicated) = check_duplicates(&transitions_keys) {
-        // Error if a transition is not in the states list
-        return Err(format!("duplicate transition [{}]", duplicated));
+        Err(format!("duplicate transition [{}]", duplicated))
     } else if let Some(found) = transitions_keys.iter().find(|el| m.finals.contains(*el)) {
-        // Error if a transition is not in the states list
-        return Err(format!("transition [{}] is a final state and cannot have transition", found));
+        Err(format!("transition [{}] is a final state and cannot have transition", found))
     } else if let Some(not_in) = check_contains_all(&transitions_keys, &not_final_states) {
-        // Error if a transition is not in the states list
-        return Err(format!("transition [{}] is not part of states", not_in));
+        Err(format!("transition [{}] is not part of states", not_in))
     } else if let Some(not_in) = check_contains_all(&not_final_states, &transitions_keys) {
-        // Error if a state (not final) doesn't have transition implementation
-        return Err(format!("state [{}] is not implemented in transitions", not_in));
+        Err(format!("state [{}] is not implemented in transitions", not_in))
+    } else if m.transitions.iter().all(|(_, trs)| trs.iter().all(|tr| !m.finals.contains(&tr.to_state))) {
+        Err(format!("no final state is used in any transition"))
+    } else {
+        m.transitions.iter().try_for_each(|(k, v)| check_state_transitions(m, k, v))
     }
-
-    let mut found_final = false;
-
-    for (key, value) in &m.transitions {
-        // for item in value.iter() {
-        for i in 0..(value.len()) {
-            //for each transition in HashMap<String, Vec<Transition>>
-
-            //check double read on same symbol
-            for j in (i + 1)..(value.len()) {
-                if value[i].read == value[j].read {
-                    return Err(format!("in transition [{}] there is multiple statement for read [{}]", key, value[i].read));
-                }
-            }
-
-            //check if final is present
-            if !found_final && m.finals.iter().position(|x| x == &value[i].to_state) != None {
-                found_final = true;
-            }
-
-            // -action can only be RIGHT or LEFT
-            if !(value[i].action == "LEFT" || &value[i].action == "RIGHT") {
-                return Err(format!("statement action [{}] from [{}] read [{}] should be [RIGHT] or [LEFT] only", value[i].action, key, value[i].read));
-            }
-
-            // -make sure "to_state" statements use only stuff from "states"
-            if m.states.iter().position(|x| x == &value[i].to_state) == None {
-                return Err(format!("statement to_state [{}] from [{}] read [{}] is not part of states", value[i].to_state, key, value[i].read));
-            }
-
-            // -make sure read statements use only stuff from "alphabet"
-            if m.alphabet.iter().position(|x| x == &value[i].read) == None {
-                return Err(format!("statement read [{}] from [{}] read [{}] is not part of alphabet", value[i].read, key, value[i].read));
-            }
-        }
-    }
-
-    Ok(())
 }
--- src/machine_runner.rs
+++ src/machine_runner.rs
@@ -7,42 +7,34 @@ use tailcall::tailcall;
 const TRANSITION_NOT_FOUND: &str = "Transition not found";
 const UNKNOWN_ACTION: &str = "Unknown action";
 
-fn machine_check_transitions(state: &Vec<Transition>, read: char) -> Result<&Transition, ()> {
-    for t in state.iter() {
-        if t.read == read {
-            return Ok(t);
-        }
-    }
-    return Err(());
+fn get_transition<'a>(state: (&String, &'a Vec<Transition>), read: char) -> Result<&'a Transition, String> {
+    state.1.iter()
+        .find(|item| item.read == read)
+        .ok_or(format!("State [{}] has not transition for read [{}], Aborting", state.0, read))
 }
 
 #[tailcall]
 fn machine_step(m: &Machine, ribbon: String, index: usize, state: (&String, &Vec<Transition>)) -> Result<String, String> {
     let c: char = ribbon.chars().nth(index).unwrap();
 
-    let get_transition: Result<&Transition, ()> = machine_check_transitions(state.1, c);
-    match get_transition {
-        Ok(t) => {
-            let left = &ribbon[..index];
-            let right = &ribbon[index + 1..];
+    let transition = get_transition(state, c)?;
+    let left = &ribbon[..index];
+    let right = &ribbon[index + 1..];
 
-            let colored_ribbon: String = format!("{}{}{}", left, c.to_string().red(), right);
-            println!("{:^15} |{:^23}R{:^3}>{:^23}W{:^3}|{:^8}", colored_ribbon, state.0, t.read, t.to_state, t.write, t.action);
+    let colored_ribbon: String = format!("{}{}{}", left, c.to_string().red(), right);
+    println!("{:^15} |{:^23}R{:^3}>{:^23}W{:^3}|{:^8}", colored_ribbon, state.0, transition.read, transition.to_state, transition.write, transition.action);
 
-            if m.finals.contains(&t.to_state) {
-                return Ok(format!("{}{}{}", left, t.write, right));
-            }
+    if m.finals.contains(&transition.to_state) {
+        return Ok(format!("{}{}{}", left, transition.write, right));
+    }
 
-            let new_state = m.transitions.get_key_value(&t.to_state).expect(TRANSITION_NOT_FOUND);
-            match (t.action.as_str(), index) {
-                ("LEFT", 0) => machine_step(m, format!("{}{}{}{}", m.blank, left, t.write, right), 0, new_state),
-                ("LEFT", _) => machine_step(m, format!("{}{}{}", left, t.write, right), index - 1, new_state),
-                ("RIGHT", i) if i + 1 == ribbon.len() => machine_step(m, format!("{}{}{}{}", left, t.write, right, &m.blank), index + 1, new_state),
-                ("RIGHT", _) => machine_step(m, format!("{}{}{}", left, t.write, right), index + 1, new_state),
-                _ => panic!("{}", UNKNOWN_ACTION)
-            }
-        }
-        Err(()) => Err(format!("State [{}] has not transition for read [{}], Aborting", state.0, ribbon.chars().nth(index).unwrap()))
+    let new_state = m.transitions.get_key_value(&transition.to_state).expect(TRANSITION_NOT_FOUND);
+    match (transition.action.as_str(), index) {
+        ("LEFT", 0) => machine_step(m, format!("{}{}{}{}", m.blank, left, transition.write, right), 0, new_state),
+        ("LEFT", _) => machine_step(m, format!("{}{}{}", left, transition.write, right), index - 1, new_state),
+        ("RIGHT", i) if i + 1 == ribbon.len() => machine_step(m, format!("{}{}{}{}", left, transition.write, right, &m.blank), index + 1, new_state),
+        ("RIGHT", _) => machine_step(m, format!("{}{}{}", left, transition.write, right), index + 1, new_state),
+        _ => panic!("{}", UNKNOWN_ACTION),
     }
 }
 
